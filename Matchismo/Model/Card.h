52e1969fa8d3585c05970b1b552173aee5040ec3
Jeroen Wesbeek <work@osx.eu>
2013-11-05 21:27:38 +0100

    added the code from the slides of lecture 1

--- Matchismo/Model/Card.h
+++ Matchismo/Model/Card.h
@@ -6,8 +6,16 @@
 //  Copyright (c) 2013 MyCompany. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Foundation/Foundation.h>   // old style import (backwards compatible in iOS 7)
+// @import Foundation;              // new style import of an entire framework (>= iOS 7)
 
 @interface Card : NSObject
+
+@property (strong, nonatomic) NSString *contents;
+
+@property (nonatomic, getter = isChosen) BOOL chosen;
+@property (nonatomic, getter = isMatched) BOOL matched;
+
+- (int)match:(NSArray *)otherCards;
 
 @end
--- Matchismo/Model/Card.m
+++ Matchismo/Model/Card.m
@@ -8,6 +8,22 @@
 
 #import "Card.h"
 
+@interface Card()
+
+@end
+
 @implementation Card
+
+- (int)match:(NSArray *)otherCards {
+    int score = 0;
+    
+    for (Card *card in otherCards) { // fast enumeration
+        if ([card.contents isEqualToString:self.contents]) {
+            score = 1;
+        }
+    }
+    
+    return score;
+}
 
 @end
